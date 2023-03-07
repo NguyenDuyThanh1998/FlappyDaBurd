81ca399208f485ec771aaf65bf80737af7fef6a3
NguyenDuyThanh1998 <112918496+NguyenDuyThanh1998@users.noreply.github.com>
2023-03-08 00:15:20 +0700

    stuff

---
+++ Assets/Scripts/Flappy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlappyDaBurd.Core
+{
+    public class Flappy : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using FlappyDaBurd.Core;
+
+namespace FlappyDaBurd
+{
+    public class GameManager : MonoBehaviour
+    {
+
+    }
+}
